a0908cfc9940b73bdfb7edf47c916dda0ebecc3f
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-02 16:42:07 +0000

    inicializate docker - Dockerfile and commands.sh

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# our static files will be on /data/web/static
+# our media files will be on /data/web/media
 DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 
