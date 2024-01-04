76c150191e6209c55ff70e7a6c820d09a6e32e43
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-04 14:57:55 +0000

    add new models for site_setup

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from django.http.request import HttpRequest
+from site_setup.models import MenuLink, SiteSetup
 
 # Register your models here.
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin): 
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
+
+#podemos comentar isto e apenas trabalhamos com o menulinkinline e sitesetup
+#@admin.register(MenuLink)
+#class MenuLinkAdmin(admin.ModelAdmin): 
+#    list_display = 'id', 'text', 'url_or_path',
+#    list_display_links = 'id', 'text', 'url_or_path',
+#    search_fields = 'id', 'text', 'url_or_path',
+    
+# queremos criar um inline que dentro do sitesetup vão aparecer os campos dos menulinks 
+class MenuLinkInline(admin.TabularInline): 
+    model = MenuLink
+    extra = 1     
+    
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin): 
+    list_display = 'title', 'description', 
+    inlines = MenuLinkInline, 
+    
+    #impedir que ao criar um registo, o user não possa criar mais nenhum 
+    # este método já existe, se retornarmos true o Adicionar aparece, se for false não aparece
+    def has_add_permission(self, request) :
+        return not SiteSetup.objects.exists()
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -17,6 +17,33 @@ class MenuLink(models.Model):
     url_or_path = models.CharField(max_length=2048)
     #new tab-> para saber se vai abrir o url num nova tab ou nao 
     new_tab = models.BooleanField(default=False)
+    # foreign key - 1 sitesetup pode ter muitos menu links 
+    #   colocamos aqui porque o pai é o sitesetup e o menulink é que vai guardar todas as infos dele 
+    site_setup = models.ForeignKey(
+        'SiteSetup', 
+        on_delete = models.CASCADE, 
+        blank = True, 
+        null = True, 
+        default = None
+    )
 
     def __str__(self):
         return self.text
+    
+    
+class SiteSetup(models.Model): 
+    class Meta: 
+        verbose_name = 'Setup'
+        verbose_name_plural = 'Setup'
+        
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=255)
+    show_header = models.BooleanField(default=True)
+    show_search = models.BooleanField(default=True)
+    show_menu = models.BooleanField(default=True)
+    show_description = models.BooleanField(default=True)
+    show_pagination = models.BooleanField(default=True)
+    show_footer = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
