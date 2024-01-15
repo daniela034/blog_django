98d48cbd59662dbf72530c647a08f174ec8b6eb7
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-15 18:56:12 +0000

    create a new blog model - POST

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from utils import rands
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Tag(models.Model):
@@ -52,6 +54,59 @@ class Page(models.Model):
                             max_length=255)
     is_published = models.BooleanField(default=False)
     content = models.TextField()
+    
+    def save(self, *args, **kwargs): 
+        if not self.slug: 
+            self.slug = rands.new_slugify(self.title)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+    
+
+class Post(models.Model): 
+    class Meta:
+        verbose_name ='Post'
+        verbose_name_plural = 'Posts'
+        
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(unique=True, default = None, null=True, blank=True,
+                            max_length=255)
+    # resumo do nosso post
+    excerpt = models.CharField(max_length=150,)
+    
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='show the cover image inside the content')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # category - pode ter muitos posts na mesma categoria 
+    #   categoria é pai do post 
+    #   no caso do menu link é ao contrario (é o filho)
+    # neste caso precisamos de ter uma categoria para criarmos um post
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL, null=True, blank=True, default=None,
+    )
+    # tag a relação é diferente 
+    # 1 post pode ter varias tags e vice-versa
+    tags = models.ManyToManyField(Tag, blank=True,default='')
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    
     
     def save(self, *args, **kwargs): 
         if not self.slug: 
