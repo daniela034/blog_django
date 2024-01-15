a48b4899f178f5d13fc3112c88c44c65772253f5
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-15 18:14:40 +0000

    create a new model for Blog - Category

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -17,4 +17,20 @@ class Tag(models.Model):
             self.slug = rands.new_slugify(self.name)
         return super().save(*args, **kwargs)
     
+
+class Category(models.Model):
+    class Meta:
+        verbose_name ='Category'
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length = 255)
+    # slug -> texto que vai representar a tag na url (como se fosse o id da tag)
+    slug = models.SlugField(unique=True, default = None, null=True, blank=True,
+                            max_length=255)
+    
+    def save(self, *args, **kwargs): 
+        if not self.slug: 
+            self.slug = rands.new_slugify(self.name)
+        return super().save(*args, **kwargs)
+    
         
