3e26ff8ab34b5f57bdeb0fedfcf5ff0f3243cecb
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-15 18:25:47 +0000

    create a new model for Blog - "Page"

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -17,6 +17,9 @@ class Tag(models.Model):
             self.slug = rands.new_slugify(self.name)
         return super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.name
+    
 
 class Category(models.Model):
     class Meta:
@@ -33,4 +36,27 @@ class Category(models.Model):
             self.slug = rands.new_slugify(self.name)
         return super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.name
+    
         
+        
+        
+class Page(models.Model): 
+    class Meta:
+        verbose_name ='Page'
+        verbose_name_plural = 'Pages'
+        
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(unique=True, default = None, null=True, blank=True,
+                            max_length=255)
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+    
+    def save(self, *args, **kwargs): 
+        if not self.slug: 
+            self.slug = rands.new_slugify(self.title)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
