94e9e5cd326aa4f7d8653951dbfaaa7f04ede902
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-16 16:15:00 +0000

    use Pillow for image - cover post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from utils import rands
 from django.contrib.auth.models import User
+from utils.images import resize_image
 
 
 # Create your models here.
@@ -111,7 +112,18 @@ class Post(models.Model):
     def save(self, *args, **kwargs): 
         if not self.slug: 
             self.slug = rands.new_slugify(self.title)
-        return super().save(*args, **kwargs)
+            
+        current_cover_name = str(self.cover.name)
+        super_save =super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900)
+            
+        return super_save
     
     def __str__(self):
         return self.title
