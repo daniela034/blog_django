579b7751f43bead81cd474133170fcd4260a96fa
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-17 18:00:27 +0000

    create a variable for number of posts per page

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.core.paginator import Paginator 
 from blog.models import Post
 
+PER_PAGE = 9
+
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(is_published=True).order_by('-pk')
-    paginator = Paginator(posts, 9)
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
