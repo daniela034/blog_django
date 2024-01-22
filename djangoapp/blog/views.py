033f5710c7272db7cf09019dac686c500ff3cdf1
Daniela Fernandes <danielaecfernandes@hotmail.com>
2024-01-22 18:43:58 +0000

    create a new url and view for the search using a word or more in a post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator 
 from blog.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -79,5 +80,23 @@ def tag(request,slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+    
+    
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+     # title have search value or excerpt have search value
+     # or content have search value
+     # Q -> using pipe | for the or 
+    posts = (Post.objects.get_published() .filter(Q(title__icontains=search_value) | Q(excerpt__icontains=search_value) |Q(content__icontains=search_value)) )[0:PER_PAGE] #type:ignore
+
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value' : search_value,
         }
     )
